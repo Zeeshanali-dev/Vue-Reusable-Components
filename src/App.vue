71c686358071e1b758e303dbc0a3e27c64613ab5
Zeeshan Ali <134941383+Zeeshanali-dev@users.noreply.github.com>
2024-07-26 17:56:09 +0500

    src file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AmazingCalculator from './components/AmazingCalculator.vue'
+import AccordianComponent from './components/AccordianComponent.vue'
+import FormValidation from './components/FormValidation.vue'
+import ProgressStep from './components/ProgressStep.vue'
+import PasswordGenerator from './components/PasswordGenerator.vue'
+</script>
+
+<template>
+
+  <AmazingCalculator />
+  <AccordianComponent />
+  <FormValidation />
+  <ProgressStep />
+  <PasswordGenerator />
+</template>
+
+<style scoped></style>
